b82a534aa27be7d5b2d23bd34fed7078ecdb9ba8
willleework <willlee_work@126.com>
2018-07-16 15:25:04 +0800

    ScrapyPool项目建立

---
+++ Source/trunk/UnitTestProject1/ScrapyCacheUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Cache;
+using Common.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrapyCache;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ScrapyCacheUnitTest
+    {
+        [TestMethod]
+        public void CacheLoadTest()
+        {
+            string filePath = @"E:\前台缓存表配置_OClient_最新版_基础.xlsm";
+            string sheetName = "缓存表";
+
+            DataTable sheetData = ExcelUtility.ExcelToDataTable(filePath, sheetName);
+
+            ScrapyCachePool pool = new ScrapyCachePool();
+            pool.Init();
+
+            pool.Get<ClientCacheConfigTable>().LoadDatas(sheetData);
+            List<ICacheItem>items =  pool.Get<ClientCacheConfigTable>().Get("单元信息表");
+            Assert.IsTrue(items.Count > 0);
+        }
+    }
+}
