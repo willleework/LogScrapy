88ea7b6b5a47e0a7ceccb57fa5369d9a90f5937d
willleework <willlee_work@126.com>
2018-07-17 20:14:10 +0800

    V0.2 添加自定义griddata控件

---
+++ Source/trunk/Log/ILogContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    /// <summary>
+    /// 日志上下文接口
+    /// </summary>
+    public interface ILogContext
+    {
+        /// <summary>
+        /// 缓存日志
+        /// </summary>
+        LogJet LogForCache { get; }
+        /// <summary>
+        /// 任务管理日志
+        /// </summary>
+        LogJet LogForTask { get; }
+        /// <summary>
+        /// 通用日志
+        /// </summary>
+        LogJet LogForCommon { get; }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="path">配置文件路径</param>
+        void Init(string path);
+    }
+}
